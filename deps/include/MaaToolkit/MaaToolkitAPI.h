f55cb30530d916a53e76d77659db200e6a245044
Uruosu <kumocyann@gmail.com>
2024-07-18 17:44:14 +0800

    Delete deps/include directory

--- deps/include/MaaToolkit/MaaToolkitAPI.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "MaaToolkitDef.h" // IWYU pragma: export
-
-#include "Config/MaaToolkitConfig.h"
-#include "Device/MaaToolkitDevice.h"
-#include "ExecAgent/MaaToolkitExecAgent.h"
-#include "Win32/MaaToolkitWin32Window.h"
